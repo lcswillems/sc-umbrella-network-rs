889920dcd6e9bddf231054373e1078d97567becf
Rares <6453351+raresserban@users.noreply.github.com>
2023-07-10 11:19:23 +0300

    Add staking bank and staking bank module.

--- staking-bank/src/lib.rs
+++ staking-bank/src/lib.rs
@@ -3,9 +3,9 @@
 multiversx_sc::imports!();
 
 #[multiversx_sc::contract]
-pub trait StakingBank: staking_bank_module::StakingBankModule
+pub trait StakingBank:
+    staking_bank_module::StakingBankModule + staking_bank_module::events::StakingBankEventsModule
 {
     #[init]
-    fn init(&self) {
-    }
+    fn init(&self) {}
 }
